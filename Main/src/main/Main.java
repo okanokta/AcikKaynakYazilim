149093a5c66655a21266a0c8662dab54cfa3e309
Okan Okta <oktaokan56@gmail.com>
2022-10-13 15:49:54 +0300

    İki sayının toplamını bulan kod.

---
+++ Main/src/main/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author oktao
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5, b = 6, sonuc;
+        sonuc = a + b;
+        System.out.println(sonuc);
+
+    }
+
+}
